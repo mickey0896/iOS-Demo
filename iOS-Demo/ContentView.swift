7b597155930040e6d8d27c29001ac03910a94979
mickey0896 <mickey_0896@hotmail. com>
2024-09-05 22:25:38 +0700

    random menu

--- iOS-Demo/ContentView.swift
+++ iOS-Demo/ContentView.swift
@@ -8,15 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var name:String = "ไอแอม ไอรอนแมน"
+    @State var name:String = "วันนี้กินอะไรดีนะ ?"
+    @State var menu:Int = 0
     var body: some View {
         VStack {
 //            Image(systemName: "globe")
 //                .imageScale(.large)
 //                .foregroundStyle(.tint)
             Text(name)
+                .font(.largeTitle).foregroundColor(.orange).frame(width: 300,height: 300,alignment: .center)
             Button("Button") {
-                self.name = "กัปตันอเมริกา"
+                self.menu =  Int.random(in: 1...5)
+                if self.menu == 1 {
+                    self.name = "ราเมง"
+                } else if self.menu == 2{
+                    self.name = "โอยาโกะด้ง"
+                }else if self.menu == 3{
+                    self.name = "ข้่าวปั้น"
+                }else if self.menu == 4{
+                    self.name = "โอมุ"
+                }else if self.menu == 5{
+                    self.name = "ซูชิ"
+                }else {
+                    self.name = "Error"
+                }
             }
         }
         .padding()
