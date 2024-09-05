c42ce487a1779170817843c34101f5620d0f8262
mickey0896 <mickey_0896@hotmail. com>
2024-09-05 21:34:55 +0700

    Initial Commit

---
+++ iOS-Demo/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  iOS-Demo
+//
+//  Created by macbook on 3/9/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS-Demo/iOS_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_DemoApp.swift
+//  iOS-Demo
+//
+//  Created by macbook on 3/9/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct iOS_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
