5d57eef84f49ef1e97e351da93424cd7cbd951db
mickey0896 <mickey_0896@hotmail. com>
2024-09-06 23:39:40 +0700

    edit

--- iOS-Demo/ContentView.swift
+++ iOS-Demo/ContentView.swift
@@ -13,9 +13,6 @@ struct ContentView: View {
     @State var count:Int = 0
     var body: some View {
         VStack {
-//            Image(systemName: "globe")
-//                .imageScale(.large)
-//                .foregroundStyle(.tint)
             Text(name)
                 .font(.largeTitle).foregroundColor(.orange).frame(width: 300,height: 300,alignment: .center)
             Button("สุ่มเลยจ้าาา") {
